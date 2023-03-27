819fec9b61d239f1e54c160ad4c523b0668b5d5b
Bradley <bradsbluetab@gmail.com>
2023-03-26 21:02:52 -0400

    fast car lol (week4)

--- src/love/stages/sunset.lua
+++ src/love/stages/sunset.lua
@@ -4,20 +4,57 @@ return {
             ["Sunset"] = graphics.newImage(graphics.imagePath("week4/sunset")),
             ["BG Limo"] = love.filesystem.load("sprites/week4/bg-limo.lua")(), -- bg-limo
             ["Limo Dancer"] = love.filesystem.load("sprites/week4/limo-dancer.lua")(), -- limo-dancer
-            ["Limo"] = love.filesystem.load("sprites/week4/limo.lua")() -- limo
+            ["Limo"] = love.filesystem.load("sprites/week4/limo.lua")(), -- limo
+            ["Fast Car"] = graphics.newImage(graphics.imagePath("week4/fastCarLol")) -- fast-car
         }
         girlfriend = love.filesystem.load("sprites/week4/girlfriend.lua")()
         enemy = love.filesystem.load("sprites/week4/mommy-mearest.lua")()
         boyfriend = love.filesystem.load("sprites/week4/boyfriend.lua")()
         fakeBoyfriend = love.filesystem.load("sprites/boyfriend.lua")() -- Used for game over
 
+        sounds.car = {
+            love.audio.newSource("sounds/week4/carPass0.ogg", "static"),
+            love.audio.newSource("sounds/week4/carPass1.ogg", "static"),
+        }
+        sounds.car[1]:setVolume(0.7)
+        sounds.car[2]:setVolume(0.7)
+
         fakeBoyfriend.x, fakeBoyfriend.y = 350, -100
         stageImages["BG Limo"].y = 250
         stageImages["Limo Dancer"].y = -130
         stageImages["Limo"].y = 375
+
+        stageImages["Fast Car"].x = -12600
+        stageImages["Fast Car"].y = love.math.random(140, 250)
+        stageImages["Fast Car"].vx = 0
+        stageImages["Fast Car"].canDrive = true
+
         girlfriend.x, girlfriend.y = 30, -50
         enemy.x, enemy.y = -380, -10
         boyfriend.x, boyfriend.y = 340, -100
+
+        function resetFastCar()
+            stageImages["Fast Car"].x = -12600
+            stageImages["Fast Car"].y = 375 + love.math.random(140, 250)
+            stageImages["Fast Car"].vx = 0
+            stageImages["Fast Car"].canDrive = true
+        end
+
+        function fastCarDrive(dt)
+            audio.playSound(sounds.car[love.math.random(2)])
+
+            stageImages["Fast Car"].canDrive = false
+            stageImages["Fast Car"].vx = (love.math.random(170, 220) / dt) * 0.25
+            if carTimer then
+                Timer.cancel(carTimer)
+            end
+            carTimer = Timer.after(2, function()
+                resetFastCar()
+                carTimer = nil
+            end)
+
+            print("Porn balls")
+        end
     end,
 
     load = function()
@@ -34,6 +71,13 @@ return {
 
 			stageImages["Limo Dancer"]:setAnimSpeed(14.4 / (60 / bpm))
 		end
+
+        if love.math.random(500) == 100 and stageImages["Fast Car"].canDrive then
+            fastCarDrive(dt)
+        end
+
+        stageImages["Fast Car"].x = stageImages["Fast Car"].x + stageImages["Fast Car"].vx * dt
+        print(stageImages["Fast Car"].x)
     end,
 
     draw = function()
@@ -54,6 +98,7 @@ return {
 			love.graphics.translate(camera.x, camera.y)
             love.graphics.translate(camera.ex, camera.ey)
 
+            stageImages["Fast Car"]:draw()
 			girlfriend:draw()
 			stageImages["Limo"]:draw()
 			enemy:draw()
